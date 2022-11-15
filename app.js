f7376811b117e09b2dac338a8d62cf7500c62923
Bose-Myneni <117246251+Bose-Myneni@users.noreply.github.com>
2022-11-15 18:02:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 const path = require("path");
-
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const app = express();
 app.use(express.json());
 
